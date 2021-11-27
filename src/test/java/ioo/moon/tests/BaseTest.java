119175dbe0455960a07f4d2ea0269f766254b36b
ioomoon <lavrushina.maria@mail.ru>
2021-11-27 10:49:17 +0300

    added selenoid support

--- src/test/java/ioo/moon/tests/BaseTest.java
+++ src/test/java/ioo/moon/tests/BaseTest.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import ioo.moon.pages.StudentRegistrationFormPage;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest {
     StudentRegistrationFormPage StudentRegistrationFormPage = new StudentRegistrationFormPage();
@@ -14,5 +15,12 @@ public class BaseTest {
 
         Configuration.startMaximized = true;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
 }
