e45e778638825866c98d2567828ca949c87c0155
ioomoon <lavrushina.maria@mail.ru>
2021-11-27 10:30:34 +0300

    added listener

--- src/test/java/ioo/moon/tests/BaseTest.java
+++ src/test/java/ioo/moon/tests/BaseTest.java
@@ -1,6 +1,8 @@
 package ioo.moon.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import ioo.moon.pages.StudentRegistrationFormPage;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -11,5 +13,6 @@ public class BaseTest {
     static void beforeAll() {
 
         Configuration.startMaximized = true;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
